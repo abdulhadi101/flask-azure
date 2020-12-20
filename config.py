3f4eb06fa4cde72a6734df9ebff905dd76f4ba5b
Abdulhadi Ahmad <get2abdul101@yahoo.com>
2020-12-20 08:31:54 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'asuku-server.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'asuku-db'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'asuku'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'abdul101#'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'asuku'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'bkRMsirqcqWkXCJqVh03yKOAvJNevbtSIeGU3qdIbCiqw0I3p2HJg1HTANwBUO+r/AwAuz4LaLbsPXe+fXRIZw=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
